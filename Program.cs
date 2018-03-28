92b301d2f4e89fb1b8ad45d11cb5786cf0974e22
Logan Horton <logan.horton@live.com>
2018-03-28 15:59:14 +1300

    Just dump of the files
    This is just simple code, nothing too crazy. Just runs a .net app (4.5 if i remember correctly) to catch all mouse/key inputs mainly setup to catch a touchscreens inputs to figure out how a double entry issue was occuring with button interaction seemingly at random

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Key_Logger___Not_bad_OK
+{
+    class Program
+    {
+        private static int counter = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    GlobalKeyboardHook gl = new GlobalKeyboardHook();
+                    gl.KeyboardPressed += OnKeyPressed;
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+        private static void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            //Debug.WriteLine(e.KeyboardData.VirtualCode);
+
+            if (e.KeyboardData.VirtualCode != GlobalKeyboardHook.VkSnapshot)
+                return;
+
+            // seems, not needed in the life.
+            //if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.SysKeyDown &&
+            //    e.KeyboardData.Flags == GlobalKeyboardHook.LlkhfAltdown)
+            //{
+            //    MessageBox.Show("Alt + Print Screen");
+            //    e.Handled = true;
+            //}
+            //else
+
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
+            {
+                Console.WriteLine((counter++).ToString());
+                Console.WriteLine("HardwareScanCode: " + e.KeyboardData.HardwareScanCode);
+                Console.WriteLine("VirtualCode: " + e.KeyboardData.VirtualCode);
+                Console.WriteLine("Flags: " + e.KeyboardData.Flags);
+                Console.WriteLine("AdditionalInformation: " + e.KeyboardData.AdditionalInformation);
+                e.Handled = true;
+            }
+        }
+    }
+}
